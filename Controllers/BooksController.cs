06b09ff6ad0b5a5cf0a52df3930ee5cec49b0e05
Casewinter <viniciusbassilio@gmail.com>
2025-01-26 22:26:31 -0300

    refactor: remove commented-out code and update BooksController structure

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -4,34 +4,6 @@ using Books.DTOs;
 using MySQLData.Data;
 using Microsoft.EntityFrameworkCore;
 
-// namespace library.Controllers
-// {
-//     [ApiController]
-//     [Route("[controller]")]
-//     // public class BookController : ControllerBase
-//     // {
-
-//     //     private readonly LibraryContext _context;
-//     //     public BookController(LibraryContext context)
-//     //     {
-//     //         _context = context;
-//     //     }
-
-//     //     [HttpGet(Name = "books")]
-//     //     public async Task<ActionResult<IEnumerable<BooksResponse>>> Get()
-//     //     {
-//     //         var allBooks = await _context.Books.ToListAsync();
-
-//     //         if (allBooks == null || allBooks.Count == 0)
-//     //         {
-//     //             return NotFound("No books found");
-//     //         }
-
-//     //         return Ok(allBooks);
-//     //     }
-//     // }
-// }
-
 namespace Books.Contollers;
 public static class Books
 {
